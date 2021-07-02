f7cc6245827a63c6ed70146741f4b1ef4761210c
Eremo Vladislav <uladzislau.yaroma@itechart-group.com>
2021-07-02 10:48:35 +0300

    Login fix

--- UI/first-react-app/src/index.js
+++ UI/first-react-app/src/index.js
@@ -6,82 +6,96 @@ import reportWebVitals from './reportWebVitals';
 import {Button} from '@material-ui/core';
 import {TextField} from '@material-ui/core'
 
-function ClientCredentials(){
-  return {
-    clientId : "ReactWebClient",
-    clientSecret : "a6a0ece0-0052-4678-82ae-ecc8817d489d",
-    grantType : "password",
-    scope : 'openid'
-  }
-}
+let ClientCredentials = {
+  clientId : "ReactWebClient",
+  clientSecret : "a6a0ece0-0052-4678-82ae-ecc8817d489d",
+  grantType : "password",
+  scope : 'openid'
+};
 
+function LoginView(){
 
-// function LoginView(){
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
 
-//     const [username, setUsername] = useState()
-//     const [password, setPassword] = useState()
-
-//     function SendLogin(){
-//       const requestLogin = {
-//         method: "POST",
-//         headers: {
-//           'Content-Type': 'application/problem+json; charset=utf-8',
+    function SendLogin(){
+      const requestLogin = {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/problem+json; charset=utf-8',
     
-//       },
-//         body: JSON.stringify({
-//           userName: username,
-//           Password: password,
-//           ClientId: "ReactWebClient",
-//           ClientSecret: "a6a0ece0-0052-4678-82ae-ecc8817d489d",
-//           GrantType: "password",
-//           Scope : "openid"
-//         })
-//       };
-//       fetch("https://localhost:44307/Account/SignInUser", requestLogin);
-//     }
+      },
+        body: JSON.stringify({
+          userName: username,
+          Password: password,
+          ClientId: ClientCredentials.clientId,
+          ClientSecret: ClientCredentials.clientSecret,
+          GrantType: ClientCredentials.grantType,
+          Scope : ClientCredentials.scope
+        })
+      };
+      fetch("https://localhost:44307/Account/SignInUser", requestLogin);
+    }
 
-//     return(
-//       <div>
-//         <form>
-//         <div>
-//           <TextField id="LoginField" label="Username:" onChange={e => setUsername(e.target.value)}/>
-//         </div>
-//         <div>
-//           <TextField id="PasswordField" type="password" label="Password:" onChange={e=> setPassword(e.target.value)}/>
-//         </div>
-//       </form>
-//       <Button name="Confirm" variant="contained" color="primary" onClick={SendLogin}>Log In</Button>
-//       </div>
-//     );
-// }
+    return(
+      <div>
+        <form>
+        <div>
+          <TextField id="LoginField" label="Username:" onChange={e => setUsername(e.target.value)}/>
+        </div>
+        <div>
+          <TextField id="PasswordField" type="password" label="Password:" onChange={e=> setPassword(e.target.value)}/>
+        </div>
+      </form>
+      <Button name="Confirm" variant="contained" color="primary" onClick={SendLogin}>Log In</Button>
+      </div>
+    );
+}
 
 
 class LoginViewClass extends React.Component{
   constructor(props){
     super(props);
 
-    this.state = {password : ""}
+    this.state = {password : "username"}
     this.state = {username : ""}
   }
-  method SendLogin(){
-    const requestLogin = {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
   
-    },
-      body: JSON.stringify({
-        userName: this.state.username,
-        Password: this.state.password,
-        ClientId: "ReactWebClient",
-        ClientSecret: "a6a0ece0-0052-4678-82ae-ecc8817d489d",
-        GrantType: "password",
-        Scope : "openid"
-      })
-    };
-    fetch("https://localhost:44307/Account/SignInUser", requestLogin);
+  render(){
+    return(
+      <div>
+      <form>
+      <div>
+        <TextField id="LoginField" label="Username:" defaultValue={this.state.username} onChange={e => {this.setState({password : e.target.value})}}/>
+      </div>
+      <div>
+        <TextField id="PasswordField" type="password" label="Password:" onChange={e=> {this.state.password = e.target.value}}/>
+      </div>
+    </form>
+    <Button name="Confirm" variant="contained" color="primary" onClick={() => {SendLogin(this.state.username, this.state.password)}}>Log In</Button>
+    </div>
+    );
   }
+}
 
+function SendLogin(username, password){
+  alert(`${username} ${password}`)
+  const requestLogin = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+
+  },
+    body: JSON.stringify({
+      UserName: "user",
+      Password: password,
+      ClientId: "ReactWebClient",
+      ClientSecret: "a6a0ece0-0052-4678-82ae-ecc8817d489d",
+      GrantType: "password",
+      Scope : "openid"
+    })
+  };
+  fetch("https://localhost:44307/Account/SignInUser", requestLogin);
 }
 
 
@@ -101,8 +115,6 @@ class LoginViewClass extends React.Component{
 //     </div>
 //   );
 // }
-
-let ResponseFromApi = "response"
 
 function Register(props){
   return(
@@ -140,9 +152,6 @@ function Register(props){
   </div>
   );
 }
-
-let intfo;
-
 function GetUsers(){
   const requestLogin = {
     method: "GET",
@@ -162,15 +171,10 @@ function SendRegister(){
   fetch("https://localhost:44307/Account/Register}", requestRegister);
 }
 
-function UI(){
-
-}
-function tick() {
-  ReactDOM.render(
+ReactDOM.render(
     <LoginViewClass/>,
     document.getElementById('root')
   );
-}
 
 
 // If you want to start measuring performance in your app, pass a function
